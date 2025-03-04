bd8363b47ef8ca61c25861413b00314d7bb6ebc1
Jarno Malmari <jmalmari@ftml.net>
2025-03-04 20:27:43 +0200

    Red slow pulse

--- main/ledgen.c
+++ main/ledgen.c
@@ -1,9 +1,13 @@
+#include <math.h>
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "led_strip.h"
 #include "esp_log.h"
 #include "esp_err.h"
+
+#define LED_PANEL_COLUMNS 9
+#define LED_PANEL_ROWS 3
 
 #define LED_STRIP_BLINK_GPIO  2
 #define LED_STRIP_LED_NUMBERS 32
@@ -35,33 +39,51 @@ led_strip_handle_t configure_led(void)
     return led_strip;
 }
 
+uint32_t point_index(unsigned row, unsigned column)
+{
+	if (LED_PANEL_ROWS <= row || LED_PANEL_COLUMNS <= column)
+	{
+		return 1;
+	}
+	return row * LED_PANEL_COLUMNS + column;
+}
+
+void fill_row(led_strip_handle_t led_strip, unsigned row, uint32_t r, uint32_t g, uint32_t b)
+{
+	for (unsigned column = 0; column < LED_PANEL_COLUMNS; ++column)
+	{
+		ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, point_index(row, column), r, g, b));
+	}
+}
+
 void app_main(void)
 {
     led_strip_handle_t led_strip = configure_led();
-    bool led_on_off = false;
 
-    ESP_LOGI(TAG, "Start blinking LED strip");
+    ESP_LOGI(TAG, "Start Falcon LED strip");
+
+	float const speed = 2 * M_PI * 0.1;
+	unsigned const gain_min = 133;
+	unsigned const gain_max = 255;
+
     while (1)
 	{
-        if (led_on_off)
+		float t = xTaskGetTickCount() * portTICK_PERIOD_MS / 1000.0;
+		int gain = (gain_max / 2.0f) * (1.0f + sinf(t * speed));
+		if (gain < gain_min)
 		{
-            /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-            for (int i = 0; i < LED_STRIP_LED_NUMBERS; i++)
-			{
-                ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, 5, 5, 5));
-            }
-            /* Refresh the strip to send data */
-            ESP_ERROR_CHECK(led_strip_refresh(led_strip));
-            ESP_LOGI(TAG, "LED ON!");
-        }
-		else
+			gain = gain_min;
+		}
+		else if (gain_max < gain)
 		{
-            /* Set all LED off to clear all pixels */
-            ESP_ERROR_CHECK(led_strip_clear(led_strip));
-            ESP_LOGI(TAG, "LED OFF!");
-        }
+			gain = gain_max;
+		}
 
-        led_on_off = !led_on_off;
-        vTaskDelay(pdMS_TO_TICKS(500));
+
+		fill_row(led_strip, 0, gain, 0, 0);
+		fill_row(led_strip, 1, gain * 60 / 100, 0, 0);
+		fill_row(led_strip, 2, gain * 30 / 100, 0, 0);
+		ESP_ERROR_CHECK(led_strip_refresh(led_strip));
+		vTaskDelay(pdMS_TO_TICKS(30));
     }
 }
